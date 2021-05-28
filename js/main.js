69524c6f79dacc7d4e1736d1088845aa0b8d3214
kkapchin <kkapchin@yandex.ru>
2021-05-28 23:24:01 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -15,14 +15,9 @@ function checkStrLength(str, max) {
 }
 
 function getRandomFloat(min, max, quantity) {
-  if (min >= 0 && max <= 10) {
-    if (min < max) {
-      return (Math.random() * (max - min + 1) + min).toFixed(quantity);
-    }
-  }
-  return 'Invalid range. Input the numbers from 0 to 10.';
+  return (min < max) ? (Math.random() * (max - min + 1) + min).toFixed(quantity) : 'Invalid range.';
 }
 
 getRandomInt(1, 10);
 checkStrLength('строка', 5);
-getRandomFloat(1.123123, 5.2323, 1);
+getRandomFloat(-1.123123, 5.2323, 1);
