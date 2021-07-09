eb3be6a30774d1e1cf8709251227f7bce7495f4d
kkapchin <kkapchin@yandex.ru>
2021-07-09 17:19:12 +0300

    Валидация хэштегов

--- js/hashtags-validation.js
+++ js/hashtags-validation.js
@@ -1,15 +1,23 @@
 import { isEscEvent } from './utils/is-escape-event.js';
+import { isSpaceEvent } from './utils/is-space-event.js';
 
-const regKeyup = /^#[a-zа-я0-9]{1,19}$/;
-const regKeydown = /[A-Za-z0-9А-Яа-я #]$/;
+const regHashtag = /^#[a-zа-я0-9]{1,19}$/;
+const regInput = /[A-Za-z0-9А-Яа-я #]$/;
 const input = document.querySelector('.text__hashtags');
 
 function keydownHandler (evt) {
+  const hashtags = input.value.split(' ');
+  const currentHashtag = hashtags[hashtags.length -1];
   if(isEscEvent(evt)) {
     evt.stopPropagation();
   }
-  if(!regKeydown.test(evt.key)) {
+  if(!regInput.test(evt.key)) {
     evt.preventDefault();
+  }
+  if(!regHashtag.test(currentHashtag)) {
+    if(isSpaceEvent(evt)) {
+      evt.preventDefault();
+    }
   }
 }
 
@@ -17,7 +25,7 @@ function keyupHandler () {
   input.value = input.value.toLowerCase();
   const hashtags = input.value.split(' ');
   const currentHashtag = hashtags[hashtags.length -1];
-  if(!regKeyup.test(currentHashtag)) {
+  if(!regHashtag.test(currentHashtag)) {
     if(currentHashtag[0] !== '#') {
       hashtags[hashtags.length -1] = `#${  currentHashtag.substring(0, 19)}`;
       input.value = hashtags.join(' ');
