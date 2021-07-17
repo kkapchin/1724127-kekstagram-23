e57f1111eee0978e9bbdf3b4c05e5a0eec1bdb5a
kkapchin <kkapchin@yandex.ru>
2021-07-18 00:12:42 +0300

    first commit

--- js/form.js
+++ js/form.js
@@ -16,18 +16,62 @@ upload.setAttribute('enctype', 'multipart/form-data');
 file.setAttribute('accept', VALID_FILE_FORMAT.join(','));
 
 function changeHandler (evt) {
-  openPopup(form);
   const image = evt.target.files[0];
   const closeButton = document.querySelector('.img-upload__cancel');
-  closePopup(closeButton, form, () => {
-    file.value = '';
-  });
-
   const reader = new FileReader();
+  const imgSize = document.querySelector('.scale__control--value');
+  const smallerBtn = document.querySelector('.scale__control--smaller');
+  const biggerBtn = document.querySelector('.scale__control--bigger');
+
+  function smallerBtnClickHandler () {
+    switch (imgSize.value) {
+      case '100%':
+        imgSize.value = '75%';
+        preview.setAttribute('style', 'transform: scale(0.75)');
+        break;
+      case '75%':
+        imgSize.value = '50%';
+        preview.setAttribute('style', 'transform: scale(0.5)');
+        break;
+      case '50%':
+        imgSize.value = '25%';
+        preview.setAttribute('style', 'transform: scale(0.25)');
+        break;
+      case '25%':
+        break;
+    }
+  }
+
+  function biggerBtnClickHandler () {
+    switch (imgSize.value) {
+      case '100%':
+        break;
+      case '75%':
+        imgSize.value = '100%';
+        preview.setAttribute('style', 'transform: scale(1)');
+        break;
+      case '50%':
+        imgSize.value = '75%';
+        preview.setAttribute('style', 'transform: scale(0.75)');
+        break;
+      case '25%':
+        imgSize.value = '50%';
+        preview.setAttribute('style', 'transform: scale(0.5)');
+        break;
+    }
+  }
+
+  openPopup(form);
+  imgSize.value = '100%';
   reader.onload = (ev) => {
     preview.children[0].src = ev.target.result;
   };
   reader.readAsDataURL(image);
+  smallerBtn.addEventListener('click', smallerBtnClickHandler);
+  biggerBtn.addEventListener('click', biggerBtnClickHandler);
+  closePopup(closeButton, form, () => {
+    file.value = '';
+  });
 }
 
 function submitHandler () {
