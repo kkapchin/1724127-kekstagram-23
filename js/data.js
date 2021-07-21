69618562300302c6c5d7439ceadcf88cc000cc15
kkapchin <kkapchin@yandex.ru>
2021-07-21 23:43:44 +0300

    доработки

--- js/data.js
+++ js/data.js
@@ -1,49 +1,8 @@
-const RANDOM_PICTURES_COUNT = 11;
-
-const DESCRIPTIONS = [
-  'Lorem ipsum',
-  'dolor sit',
-  'amet consectetur',
-  'adipisicing elit',
-  'Accusamus, esse',
-  'error ipsam',
-  'voluptas ab',
-  'eveniet unde',
-  'exercitationem fugiat',
-  'quis eum',
-  'iste perferendis',
-  'ipsum earum iure!',
-];
-
-const COMMENTS = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
-const NAMES = [
-  'Джереми',
-  'Жозефина',
-  'Антуанетта',
-  'Крэг Хек',
-  'Гервульф',
-  'Лабета',
-  'Луна',
-  'Джейсон Стэтхэм',
-];
-
 const VALID_FILE_FORMAT = ['.png', '.jpg', '.jpeg', '.gif'];
 
 const RERENDER_DELAY = 500;
 
 export {
-  DESCRIPTIONS,
-  COMMENTS,
-  NAMES,
-  RANDOM_PICTURES_COUNT,
   VALID_FILE_FORMAT,
   RERENDER_DELAY
 };
